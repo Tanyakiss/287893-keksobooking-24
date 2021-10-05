7c461f727f3f2b7b6673a9b9e9f0788f9d8db774
Tatsiana Kisliakova <tanyakiss87@yandex.ru>
2021-10-05 19:24:16 +0300

    Add lodash script
    Add lodash script random for index array

--- js/main.js
+++ js/main.js
@@ -82,30 +82,33 @@ const LOCATION = {
 
 //описываю функцию, кот создает одно объявление
 const createAdvertising = () => {
+  const randomTitlesIndex = _.random(0, TITLES.length - 1);
+  const randomDescsIndex = _.random(0, DESCS.length - 1);
+
   return{
-    author: {
-      avatar:'img/avatars/user{{xx}}.png',
-    },
-    offer:{
-      title:offer.TITLES[i],
-      address:[
-        LOCATION.lat,
-        LOCATION.lng,
-      ],
-      price:getRandomInt(offer.PRICE.min, offer.PRICE.max),
-      type:offer.TYPES[getRandomInt(0, offer.TYPES.length - 1)],
-      rooms:getRandomInt(offer.ROOMS.min, offer.ROOMS.max),
-      guests:getRandomInt(offer.GUESTS.min, offer.GUESTS.max),
-      checkin:offer.CHECKINS[getRandomInt(0, offer.CHECKINS.length - 1)],
-      checkout:offer.CHECKOUTS[getRandomInt(0, offer.CHECKOUTS.length - 1)],
-      features:offer.FEATURES[getRandomInt(0, offer.FEATURES.length - 1)],
-      description:offer.DESCS[i],
-      photos:offer.PHOTOS[getRandomInt(0, offer.PHOTOS.length - 1)],
-    },
-    location:{
-      lat:getRandomArbitrary(LOCATION.lat.min, LOCATION.lat.max, 5),
-      lng:getRandomArbitrary(LOCATION.lng.min, LOCATION.lng.max, 5),
-    },
+      author: {
+        avatar:'img/avatars/user{{xx}}.png',
+      },
+      offer:{
+        title:offer.TITLES[randomTitlesIndex],
+        address:[
+          LOCATION.lat,
+          LOCATION.lng,
+        ],
+        price:getRandomInt(offer.PRICE.min, offer.PRICE.max),
+        type:offer.TYPES[getRandomInt(0, offer.TYPES.length - 1)],
+        rooms:getRandomInt(offer.ROOMS.min, offer.ROOMS.max),
+        guests:getRandomInt(offer.GUESTS.min, offer.GUESTS.max),
+        checkin:offer.CHECKINS[getRandomInt(0, offer.CHECKINS.length - 1)],
+        checkout:offer.CHECKOUTS[getRandomInt(0, offer.CHECKOUTS.length - 1)],
+        features:offer.FEATURES[getRandomInt(0, offer.FEATURES.length - 1)],
+        description:offer.DESCS[randomDescsIndex],
+        photos:offer.PHOTOS[getRandomInt(0, offer.PHOTOS.length - 1)],
+      },
+      location:{
+        lat:getRandomArbitrary(LOCATION.lat.min, LOCATION.lat.max, 5),
+        lng:getRandomArbitrary(LOCATION.lng.min, LOCATION.lng.max, 5),
+      },
   };
 };
 
