cfee28efcd8be79f95b29ba2afd2e7cfe76023c6
Tatsiana Kislyakova <tanyakiss87@yandex.ru>
2021-10-07 11:29:57 +0300

    Fix and so on

--- js/main.js
+++ js/main.js
@@ -89,5 +89,54 @@ const offer = {
     'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
     'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
     'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-]
+],
 }
+
+const LOCATION = {
+  lat:{
+    min:35.65000,
+    max:35.70000,
+  },
+  lng:{
+    min:139.70000,
+    max:139.80000,
+  },
+}
+
+//описываю функцию, кот создает одно объявление
+const createAdvertising = () => {
+  const randomTitlesIndex = getRandomInt(0, offer.TITLES.length - 1 );
+  const randomDescsIndex = getRandomInt(0,  offer.DESCS.length - 1 );
+
+  return{
+      author: {
+        avatar:'img/avatars/user{{xx}}.png',
+      },
+      offer:{
+        title:offer.TITLES[randomTitlesIndex],
+        address:[
+          LOCATION.lat,
+          LOCATION.lng,
+        ],
+        price:getRandomInt(offer.PRICE.min, offer.PRICE.max),
+        type:offer.TYPES[getRandomInt(0, offer.TYPES.length - 1)],
+        rooms:getRandomInt(offer.ROOMS.min, offer.ROOMS.max),
+        guests:getRandomInt(offer.GUESTS.min, offer.GUESTS.max),
+        checkin:offer.CHECKINS[getRandomInt(0, offer.CHECKINS.length - 1)],
+        checkout:offer.CHECKOUTS[getRandomInt(0, offer.CHECKOUTS.length - 1)],
+        features:offer.FEATURES[getRandomInt(0, offer.FEATURES.length - 1)],
+        description:offer.DESCS[randomDescsIndex],
+        photos:offer.PHOTOS[getRandomInt(0, offer.PHOTOS.length - 1)],
+      },
+      location:{
+        lat:getRandomArbitrary(LOCATION.lat.min, LOCATION.lat.max, 5),
+        lng:getRandomArbitrary(LOCATION.lng.min, LOCATION.lng.max, 5),
+      },
+  };
+};
+
+let adsArr = [];
+for (let i = 0; i < TOTAL_ADS; i++) {
+  adsArr[i] = createAdvertising(i);
+}
+
