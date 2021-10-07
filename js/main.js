e6a3f10d474b11a9b0f579875b57643da23077e8
Tatsiana Kislyakova <tanyakiss87@yandex.ru>
2021-10-07 11:26:01 +0300

    Fix fix fix

--- js/main.js
+++ js/main.js
@@ -89,3 +89,4 @@ const offer = {
     'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
     'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
     'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+]
