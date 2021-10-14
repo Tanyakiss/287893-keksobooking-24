50ef8828fa16456d9a494611b629945570a5c3a5
Tatsiana Kisliakova <tanyakiss87@yandex.ru>
2021-10-14 19:50:23 +0300

    Add new module popup

--- js/util.js
+++ js/util.js
@@ -19,3 +19,41 @@ function getRandomArbitrary(min, max, numberRound) {
 }
 
 export {getRandomArbitrary};
+
+const getRussianCase = (integer, russianWord) => {
+  const remainder = integer % 10;
+  if (integer > 10 && integer < 20) {
+    return russianWord[2];
+  } else if (remainder > 1 && remainder < 5) {
+    return russianWord[1];
+  } else if (remainder === 1) {
+    return russianWord[0];
+  }
+  return russianWord[2];
+};
+
+export {getRussianCase};
+
+const getRussianGenitiveCase = (integer, russianWord) => {
+  const remainder = integer % 10;
+  return remainder === 1 && integer !== 11 ? russianWord[0] : russianWord[1];
+};
+
+export {getRussianGenitiveCase};
+
+const getAccommodationType = (type) => {
+  switch (type) {
+    case 'flat':
+      return 'Квартира';
+    case 'bungalow':
+      return 'Бунгало';
+    case 'house':
+      return 'Дом';
+    case 'palace':
+      return 'Дворец';
+    case 'hotel':
+      return 'Отель';
+  }
+};
+
+export {getAccommodationType};
