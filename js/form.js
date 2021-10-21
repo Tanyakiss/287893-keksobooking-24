7f825f3fe20ac9b996f3dcc3a3d9a8a6c933d323
Tatsiana Kisliakova <tanyakiss87@yandex.ru>
2021-10-21 19:08:01 +0300

    Тип жилья, заезд выезд

--- js/form.js
+++ js/form.js
@@ -3,15 +3,56 @@ const MAX_TITLE_LENGTH = 100;
 const MIN_PRICE = 0;
 const MAX_PRICE = 1000000;
 const MAX_ROOM_NUMBER = 100;
+const MIN_BUNGALOW_PRICE = 0;
+const MIN_FLAT_PRICE = 1000;
+const MIN_HOTEL_PRICE = 3000;
+const MIN_HOUSE_PRICE = 5000;
+const MIN_PALACE_PRICE = 10000;
 
 const adForm = document.querySelector('.ad-form');
+const adFormFields = Array.from(adForm.querySelectorAll('fieldset'));
 const adTitle = adForm.querySelector('#title');
 const adPrice = adForm.querySelector('#price');
+const adAccommodationType = adForm.querySelector('#type');
+const adCkeckin = adForm.querySelector('#timein');
+const adCkeckout = adForm.querySelector('#timeout');
 const adRoomNumber = adForm.querySelector('#room_number');
 const adGuestNumber = adForm.querySelector('#capacity');
+const filterForm = document.querySelector('.map__filters');
 
 
-adGuestNumber.querySelector('option[value="1"]').setAttribute('selected', 'selected');
+const filterFormFields = Array.from(filterForm.children);
+
+const useInactivePageState = () => {
+  adForm.classList.add('ad-form--disabled');
+  filterForm.classList.add('map__filters--disabled');
+  adFormFields.forEach((item) => {
+    item.disabled = true;
+  });
+  filterFormFields.forEach((item) => {
+    item.disabled = true;
+  });
+};
+
+const useActivePageState = () => {
+  adForm.classList.remove('ad-form--disabled');
+  filterForm.classList.remove('map__filters--disabled');
+  adFormFields.forEach((item) => {
+    item.disabled = false;
+  });
+  filterFormFields.forEach((item) => {
+    item.disabled = false;
+  });
+};
+
+
+const getSameTimeIn = () => {
+  adCkeckin.value = adCkeckout.value;
+};
+
+const getSameTimeOut = () => {
+  adCkeckout.value = adCkeckin.value;
+};
 
 const onSelectRoomChange = () => {
   const roomsValue = adRoomNumber.value;
@@ -53,6 +94,38 @@ adPrice.addEventListener('input', () => {
   adPrice.reportValidity();
 });
 
+adAccommodationType.addEventListener('change', () => {
+  switch (adAccommodationType.value) {
+    case 'flat':
+      adPrice.setAttribute('min', MIN_FLAT_PRICE);
+      adPrice.setAttribute('placeholder', MIN_FLAT_PRICE);
+      break;
+    case 'bungalow':
+      adPrice.setAttribute('min', MIN_BUNGALOW_PRICE);
+      adPrice.setAttribute('placeholder', MIN_BUNGALOW_PRICE);
+      break;
+    case 'house':
+      adPrice.setAttribute('min', MIN_HOUSE_PRICE);
+      adPrice.setAttribute('placeholder', MIN_HOUSE_PRICE);
+      break;
+    case 'palace':
+      adPrice.setAttribute('min', MIN_PALACE_PRICE);
+      adPrice.setAttribute('placeholder', MIN_PALACE_PRICE);
+      break;
+    case 'hotel':
+      adPrice.setAttribute('min', MIN_HOTEL_PRICE);
+      adPrice.setAttribute('placeholder', MIN_HOTEL_PRICE);
+      break;
+  }
+});
+
+adGuestNumber.querySelector('option[value="1"]').setAttribute('selected', 'selected');
 
 adRoomNumber.addEventListener('change', onSelectRoomChange);
 adGuestNumber.addEventListener('change', onSelectRoomChange);
+
+adCkeckin.addEventListener('change', getSameTimeOut);
+adCkeckout.addEventListener('change', getSameTimeIn);
+
+useInactivePageState();
+useActivePageState();
