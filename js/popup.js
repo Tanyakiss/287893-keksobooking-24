68bf237ff75e3f7d4bcce3bace11e56124a8744b
Tatsiana Kisliakova <tanyakiss87@yandex.ru>
2021-11-01 12:15:48 +0300

    Fix popup bags

--- js/popup.js
+++ js/popup.js
@@ -7,28 +7,28 @@ const adCardTemplate = document.querySelector('#card').content.querySelector('.p
 
 const createCustomPopup = (avatar, offer) => {
   const adElement = adCardTemplate.cloneNode(true);
-  avatar.title
-  ? adElement.querySelector('.popup__title').textContent = offer.title
-  : adElement.querySelector('.popup__title').classList.add('visually-hidden');
-  offer.address
-  ? adElement.querySelector('.popup__text--address').textContent = offer.address
-  : adElement.querySelector('.popup__text--address').classList.add('visually-hidden');
-  offer.price
-  ? adElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`
-  : adElement.querySelector('.popup__text--price').classList.add('visually-hidden');
-  offer.type
-  ? adElement.querySelector('.popup__type').textContent = getAccommodationType(offer.type)
-  : adElement.querySelector('.popup__type').classList.add('visually-hidden');
-  offer.rooms && offer.guests
-  ? adElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} ${getRussianCase(offer.rooms, ROOMS)} для ${offer.guests} ${getRussianGenitiveCase(offer.guests, GUESTS)}`
-  : adElement.querySelector('.popup__text--capacity').classList.add('visually-hidden');
-  offer.checkin && offer.checkout
-  ? adElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`
-  : adElement.querySelector('.popup__text--time').classList.add('visually-hidden');
-  if (offer.features) {
-    const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
-    adElement.querySelectorAll('.popup__feature').forEach((item) => {
-      const modifier = item.classList[1];
+    avatar.title
+    ? adElement.querySelector('.popup__title').textContent = offer.title
+    : adElement.querySelector('.popup__title').classList.add('visually-hidden');
+    offer.address
+    ? adElement.querySelector('.popup__text--address').textContent = offer.address
+    : adElement.querySelector('.popup__text--address').classList.add('visually-hidden');
+    offer.price
+    ? adElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`
+    : adElement.querySelector('.popup__text--price').classList.add('visually-hidden');
+    offer.type
+    ? adElement.querySelector('.popup__type').textContent = getAccommodationType(offer.type)
+    : adElement.querySelector('.popup__type').classList.add('visually-hidden');
+    offer.rooms && offer.guests
+    ? adElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} ${getRussianCase(offer.rooms, ROOMS)} для ${offer.guests} ${getRussianGenitiveCase(offer.guests, GUESTS)}`
+    : adElement.querySelector('.popup__text--capacity').classList.add('visually-hidden');
+    offer.checkin && offer.checkout
+    ? adElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`
+    : adElement.querySelector('.popup__text--time').classList.add('visually-hidden');
+    if (offer.features) {
+      const modifiers = offer.features.map((feature) => `popup__feature--${feature}`);
+      adElement.querySelectorAll('.popup__feature').forEach((item) => {
+        const modifier = item.classList[1];
       if (!modifiers.includes(modifier)) {
         item.remove();
       }
